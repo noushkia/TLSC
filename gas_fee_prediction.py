9adb84b7cf11bfaeb2232c621abb3028fdbf4682
noushkia <kianoosharshi@gmail.com>
2023-10-29 20:28:04 +0800

    Add total gas fee calculation

--- gas_fee_prediction.py
+++ gas_fee_prediction.py
@@ -7,11 +7,11 @@ BLOCK_SIZE_LIMIT = 30_000_000
 ETH_TO_GWEI = 1e9
 
 
-def calculate_base_fee(curr_base_fee: float, _block_range: int) -> float:
+def calculate_fee(curr_base_fee: float, _block_range: int, max_priority_fee_ratio: float = 0) -> float:
     _total_fee = 0
     for i in range(_block_range):
         curr_base_fee *= (1 + MAX_INCREASE_RATE / 100)
-        _total_fee += curr_base_fee
+        _total_fee += curr_base_fee * (1 + max_priority_fee_ratio)
         if i in [1, 10, 50]:
             print(f"{i} blocks: {_total_fee}")
             print(f"cost for full block utilization: {_total_fee * BLOCK_SIZE_LIMIT / ETH_TO_GWEI} ETH")
@@ -21,6 +21,12 @@ def calculate_base_fee(curr_base_fee: float, _block_range: int) -> float:
 if __name__ == "__main__":
     initial_base_fee = 20
     block_range = 100
-    total_fee = calculate_base_fee(20, 100)
+    total_fee = calculate_fee(20, 100)
+    print("Base fees for different block ranges:")
+    print(f"{block_range} blocks: {total_fee}")
+    print(f"cost for full block utilization: {total_fee * BLOCK_SIZE_LIMIT / ETH_TO_GWEI} ETH")
+
+    total_fee = calculate_fee(20, 100, 0.05)  # 5% max fee
+    print("Total fees for different block ranges:")
     print(f"{block_range} blocks: {total_fee}")
     print(f"cost for full block utilization: {total_fee * BLOCK_SIZE_LIMIT / ETH_TO_GWEI} ETH")
